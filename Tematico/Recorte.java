548f977e45c150dffb73cfa7f4d19473170bbeb7
Matias Grondona <44991886+MatiasGrondona@users.noreply.github.com>
2021-04-20 10:33:57 -0300

    Add files via upload

---
+++ Tematico/Recorte.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Recorte here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Recorte
+{
+    // instance variables
+    double 
+
+    /**
+     * Constructor for objects of class Recorte
+     */
+    public Recorte()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
